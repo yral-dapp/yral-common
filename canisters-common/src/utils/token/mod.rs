ec818a480a28d7c04c38e61b5254d6f03a9c61c5
tushar-saxena-yral <tushar@gobazzinga.io>
2025-02-04 22:51:47 +0530

    chore: lint fix

--- canisters-common/src/utils/token/mod.rs
+++ canisters-common/src/utils/token/mod.rs
@@ -163,7 +163,7 @@ impl<const A: bool> Canisters<A> {
 
                 let withdrawal_state = if bal_info.withdrawable == 0usize {
                     WithdrawalState::NeedMoreEarnings(
-                        (bal_info.net_airdrop_reward - bal_info.balance) + 1,
+                        (bal_info.net_airdrop_reward - bal_info.balance) + 1usize,
                     )
                 } else {
                     WithdrawalState::Value(bal_info.withdrawable)
