3e3166929c525e2f1f7488fc0282b6c890e17a07
komal-sai-yral <komalsai@gobazzinga.io>
2025-03-05 19:01:43 +0530

    pnd event changes

---
+++ metrics/src/metrics/tides_turned.rs
@@ -0,0 +1,30 @@
+use candid::{Nat, Principal};
+use serde::Serialize;
+
+use super::sealed_metric::SealedMetric;
+
+#[derive(Serialize, Clone, Debug)]
+pub struct TidesTurned {
+    pub user_principal: Principal,
+    pub user_canister: Principal,
+    pub is_registered: bool,
+    pub staked_amount: u64,
+    pub round_num: u64,
+    pub user_pumps: u64,
+    pub user_dumps: u64,
+    pub round_pumps: u64,
+    pub round_dumps: u64,
+    pub cumulative_pumps: u64,
+    pub cumulative_dumps: u64,
+    pub token_root: Principal,
+}
+
+impl SealedMetric for TidesTurned {
+    fn tag(&self) -> String {
+        "tides_turned".into()
+    }
+
+    fn user_id(&self) -> Option<String> {
+        Some(self.user_canister.to_text())
+    }
+}
